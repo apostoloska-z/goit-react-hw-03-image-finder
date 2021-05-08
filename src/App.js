e8e54906db1de1b8c12183008b721c723ab9345d
Zoia Apostoloska <zoiaapostoloska@gmail.com>
2021-05-08 12:46:55 +0300

    adds initial files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+import Searchbar from "./Components/Searchbar/"
+
+class App extends Component {
+
+  render() {
+    return (
+      <Searchbar/>
+    )
+  }
+}
+
+export default App;
